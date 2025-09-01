6f0a1fef80abff7f5477db1510aad24a46003430
Jose Acosta <jose.m.acosta1996@gmail.com>
2025-09-01 09:52:10 -0600

    second commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,31 +2,60 @@ import Hexagon from "@/components/hexagon";
 import { ThemedView } from "@/components/ThemedView";
 import { ScrollView } from "react-native";
 
+const randomNumber = () => {
+  return Math.floor(Math.random() * 100);
+};
+
 export default function HomeScreen() {
   return (
     <ScrollView style={{ flex: 1 }}>
-      <ThemedView
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        {Array.from({ length: 30 }).map((_, index) => (
-          <Hexagon
-            key={index}
-            hexagonSize={50}
-            fillPercentage={100}
-            cornerRadius={2.5}
-            iconWidth={20}
-            iconHeight={20}
-            strokeWidth={4}
-            activeColor={["#6CE625"]}
-            fillColor={["#489919", "#46bb04"]}
-            borderColor={["#6CE625", "#30E6FF"]}
-          />
-        ))}
+      <ThemedView style={{ flex: 1, flexDirection: "row" }}>
+        <ThemedView
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          {Array.from({ length: 50 }).map((_, index) => (
+            <Hexagon
+              key={index}
+              hexagonSize={50}
+              fillPercentage={randomNumber()}
+              cornerRadius={2.5}
+              iconWidth={20}
+              iconHeight={20}
+              strokeWidth={4}
+              activeColor={["#6CE625"]}
+              fillColor={["#489919", "#46bb04"]}
+              borderColor={["#6CE625", "#30E6FF"]}
+            />
+          ))}
+        </ThemedView>
+        <ThemedView
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          {Array.from({ length: 50 }).map((_, index) => (
+            <Hexagon
+              key={index}
+              hexagonSize={50}
+              fillPercentage={randomNumber()}
+              cornerRadius={2.5}
+              iconWidth={20}
+              iconHeight={20}
+              strokeWidth={4}
+              activeColor={["#6CE625"]}
+              fillColor={["#489919", "#46bb04"]}
+              borderColor={["#6CE625", "#30E6FF"]}
+            />
+          ))}
+        </ThemedView>
       </ThemedView>
     </ScrollView>
   );
